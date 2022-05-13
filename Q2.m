8450873490b424b01c7019ed48e2c2ea6e3f7b82
Rafael Modesto <rafael.moura@ga.ita.br>
2022-05-13 18:55:00 -0300

    Add files via upload

--- Q2.m
+++ Q2.m
@@ -1,43 +1,37 @@
 % 2c)
-v = 138.9;
-launch_angle = 25*pi/180;
-gamma = linspace(launch_angle,0,100);
-
-for n = [3.3 3 2.7 2.4]
-    r_gamma = r(gamma,v,n);
-
-    x = r_gamma.*sin(gamma);
-    y = r(0,v,n) - r_gamma.*cos(gamma)+300;
-
-    plot(x,y)
+range = 1.6 + 0.2*[1:4];
+for n = range
+    f2 = @(t, pos)fun(t, pos, n);
+    [t, pos] = ode45(f2,[0 8],[0 457.2 -deg2rad(25)]);
+    
+    p = plot(pos(:,1),pos(:,2));
     hold on
 end
-
-lim_y = max(y)*1.05;
-lim_x = max(x)*1.05;
-axis equal
 yline(300,'k--')
-text(0.65*lim_x,300+lim_y*0.03,'Altitude de obstáculos')
+text(20,310,'Altitude de obstáculos')
 yline(457.2,'k--')
-text(0.02*lim_x,457.2+lim_y*0.03,'Altitude operacional')
-legend("n = 3.3", "n = 3", "n = 2.7", ...
-    "n = 2.4", "","",...
-    'Location','best')
-ylabel('Altitude (m)')
-xlabel('Posição (m)')
-title('Trajetórias críticas de acordo com o fator de carga')
-
+text(20,467.2,'Altitude operacional')
+ylim([200,500])
+legs = [string("n = " + range) '' ''];
+legend(legs,"Location","best")
+ylabel("Altura")
+xlabel("Distância")
+title("Trajetória crítica de acordo com o fator de carga")
 hold off
 
 % 2d)
-for n = linspace(2.7,3,30000)
-    if abs(r(0,v,n) - r(launch_angle,v,n)*cos(launch_angle)+300-457.2) < 0.001
-        n_max = n
+for n = linspace(2,2.2,1000)
+    f2 = @(t, pos)fun(t, pos, n);
+    [t, pos] = ode45(f2,[0 8],[0 457.2 -deg2rad(25)]);
+    if abs(min(pos(:,2)) - 300) < 0.01
+        n
     end
 end
 
 % Funções utilizadas
-function radius = r(angle,vel,n_z)
-    g = 9.81;
-    radius = vel^2./(g*(n_z-cos(angle)));
+function pos_P = fun(~, pos, n)
+    g = 9.8; v = 138.9;
+    gamma = pos(3);
+    pos_P = [v.*cos(gamma) v.*sin(gamma) g*(n-cos(gamma))/v]';
 end
+
